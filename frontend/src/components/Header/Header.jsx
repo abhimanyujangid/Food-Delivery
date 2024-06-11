2fd5f915c1a9bdbf575d33df8a9e88b26321d2de
Abhimanyu Jangid <abhimanyujangid79@gmail.com>
2024-06-11 22:41:16 +0400

    added nav-bar and home page

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div>
+      <div className='header'>
+        <div className="header-contents">
+          <h2>Order you favourite food here</h2>
+          <p>Choose from a dicers menu featuinr a delectable array</p>
+          <button>View Menu</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
